e8d491f4822a5e42f1c733d6d197030c77901f82
Dekim12 <Dekim12@github.com>
2018-10-05 23:36:38 +0200

    version 0.5 commit

--- src/index.js
+++ src/index.js
@@ -4,3 +4,23 @@ import './styles/style.sass';
  * Example of images import:
  * import './images/header/pencil.png';
 */
+let arrEvents = document.getElementsByClassName('first')[0];
+let modalW = document.getElementsByClassName('modal')[0];
+let headerFixed = document.getElementsByClassName('fixed_block')[0];
+let btnClose = document.getElementsByClassName('btnClose')[0];
+
+arrEvents.addEventListener('click', function() {
+  modalW.style.display = 'block';
+  headerFixed.style.position = 'relative';
+});
+
+btnClose.addEventListener('click', function() {
+  modalW.style.display = 'none';
+  headerFixed.style.position = 'fixed';
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target == modalW) {
+    modalW.style.display = 'none';
+  }
+});
